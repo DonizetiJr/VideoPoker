a29a169fcf0e79171a89d8aa0ef799b3dbe4756e
DonizetiJr <dcs.junior22@gmail.com>
2017-04-04 19:15:03 -0300

    Initiating Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,32 @@
+import java.util.Stack;
+
 public class Player {
-
-	/**
-	* @author Edylson T. & Donizeti Jr.
-	*/
+	private List<Card> hand;
+	private Pocket pocket;
 	
-    public Player() {
-
-    }
+	public Player() {
+		this(5);
+	}
+	
+	public Player(int handSize){
+		this.hand = new Card[handSize];
+		this.pocket = new Pocket();
+	}
+	
+	public int bet(int betValue) {
+		
+	}
+	
+	public String showHand() {
+		
+	}
+	
+	public Card pullCard(Stack<Card> deck) {
+		deck.pop();
+	}
+	
+	public Card dropCard(int cardPos) {
+		return hand[cardPos];
+	}
+	
 }
--- src/VideoPoker.java
+++ src/VideoPoker.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.Stack;
+
 public class VideoPoker {
 
 	/**
@@ -6,6 +9,28 @@ public class VideoPoker {
 	*/
 	
     public static void main(String[] args) {
-        
+    	Stack<Card> deck = new Stack<Card>();
+    	
+    	for (Card.Suit s : Card.Suit.values()) {
+    		for (Card.Rank r : Card.Rank.values()) {
+    			deck.push(new Card(r, s));
+    		}
+    	}
+    	
+    	Collections.shuffle(deck);
+    	
+    	Pocket pocket = new Pocket();
+    	
+    	int aposta;
+    	Card hand[] = new Card[5];
+    	
+    	do {
+    		System.out.print("Digite o valor de sua aposta: ");
+    		aposta = EntradaTeclado.leInt();
+    		
+    		
+    	} while ();
+    	
+    	
     }
 }
