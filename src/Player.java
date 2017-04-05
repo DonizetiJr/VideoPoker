2530529b261836ba121185da380b6a3a17753441
DonizetiJr <dcs.junior22@gmail.com>
2017-04-05 19:22:08 -0300

    Init main

--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+import Card.Rank;
+
 public class Player {
     private int maxHandSize;
 	private ArrayList<Card> hand;
@@ -24,6 +26,69 @@ public class Player {
 
 	public void bet(int betValue) {
         this.pocket.wage(betValue);
+	}
+	
+	private int verifyHand() {
+		int[] ranks = new int[13];
+		int[] suits = new int[4];
+		int sequence = 0;
+		boolean equalSuits = false;
+		
+		for (Card c : this.hand) {
+			ranks[c.getValue().ordinal()]++;
+			suits[c.getSuit().ordinal()]++;
+		}
+		
+		for (int rankFreq : ranks) {
+			if (rankFreq == 0)
+				sequence = 0;
+			sequence++;
+			
+			if (sequence == 5) break;
+		}
+		
+		for (int suitFreq : suits)
+			if (suitFreq == 5)
+				equalSuits = true;
+		
+		if (equalSuits) {
+			if (sequence == 5) {
+				if (ranks[Card.Rank.ACE.ordinal()] == 1)
+					return 200;
+				else
+					return 100;
+			}
+			
+			return 10;
+		}
+		
+		if (sequence == 5)
+			return 5;
+		
+		int pair = 0;
+		int triple = 0;
+		for (int i = 0; i < ranks.length; i++) {
+			if (ranks[i] == 4)
+				return 50;
+			
+			if (ranks[i] == 2) pair++;
+			if (ranks[i] == 3) triple++;
+		}
+		
+		if (triple == 1)
+			if (pair == 1)
+				return 20;
+			else
+				return 2;
+		
+		if (pair == 2)
+			return 1;
+		
+		return 0;
+	}
+	
+	public void receiveCredits(int credits) {
+		this.pocket.winCredits();
 	}
 
 	public String showHand() {
